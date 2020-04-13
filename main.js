72cd48b616cd935a914f8922107ab14105f1fd2b
Thoa Nguyen <thoanguyen290705@gmail.com>
2020-04-13 16:26:54 +0300

    Expense Tracker

---
+++ main.js
@@ -0,0 +1,91 @@
+const balance = document.getElementById('balance'); 
+const money_plus = document.getElementById('money-plus'); 
+const money_minus = document.getElementById('money-minus'); 
+const list = document.getElementById('list'); 
+const form = document.getElementById('form'); 
+const text = document.getElementById('text'); 
+const amount = document.getElementById('amount'); 
+
+const dummyTransactions = [
+    {
+        id:1,
+        text:'Flower',
+        amount:-20
+    },
+    {
+        id:2,
+        text:'Salary',
+        amount:300
+    },
+    {
+        id:3,
+        text:'Book',
+        amount:-10
+    },
+   
+]
+let transactions = dummyTransactions;
+//add transaction 
+function addTransaction(e){
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        console.log('fsdfsad')
+    } else{
+        const transaction = {
+            id: generateID(),
+            text:text.value, 
+            amount:+amount.value
+        };
+        transactions.push(transaction);
+        addTransactionDOM(transaction);
+        updateValues;
+        text.value = '';
+        amount.value = '';   
+    }
+e.preventDefault();
+}
+function generateID(){
+    return Math.floor(Math.random()*10000000)
+}
+//add transaction to DOM list 
+function addTransactionDOM(transaction){
+    //Get sign 
+    const sign = transaction.amount < 0?'-':'+';
+    const item = document.createElement('li');
+    //add Class based on value 
+    item.classList.add(transaction.amount < 0?'minus':'plus');
+    item.innerHTML =  `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">X</button>
+    `;
+    list.appendChild(item)
+}
+//Update the balance,income and expense 
+function updateValues(){
+    const amounts = transactions.map(transaction=>transaction.amount);
+    const total = amounts.reduce((acc,item)=> acc+=item,0).toFixed(2);
+    const income = amounts
+                .filter(item => item >0)
+                .reduce((acc,item)=> acc+=item,0).toFixed(2);
+      
+    const expense = amounts
+                    .filter(item=>item<0)
+                    .reduce((acc,item)=>acc+=item,0).toFixed(2)
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+
+}
+
+//Remove transaction by ID
+function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id !== id)
+    init()
+}
+//Innit app 
+function init(){
+    list.innerHTML = ''; 
+    transactions.forEach((addTransactionDOM));
+    updateValues();
+}
+init();
+form.addEventListener('submit',addTransaction)
